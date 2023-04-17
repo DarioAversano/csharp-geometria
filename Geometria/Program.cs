80b1a90316c0ac41fbe373e547172ac227eea91a
darioaversano <risusveloce@gmail.com>
2023-04-17 16:14:20 +0200

    Commit Rettangoli

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
+using RettangoliVari;
+
 Console.WriteLine("Hello, World!");
+
+
+
+//Rettangoli
+
+Rettangolo rettangoloClassico = new Rettangolo(20, 10);
+Console.WriteLine("ATTRIBUTI DEL RETTANGOLO CLASSICO: ");
+Console.WriteLine("La Base del rettangolo classico è di: " + rettangoloClassico.baseRettangolo + " Cm");
+Console.WriteLine("L'altezza del rettangolo classico è di: " + rettangoloClassico.altezzaRettangolo + " Cm");
+
+Rettangolo rettangoloGrande = new Rettangolo(30, 15);
+Console.WriteLine("ATTRIBUTI DEL RETTANGOLO GRANDE: ");
+Console.WriteLine("La Base del rettangolo piccolo è di: " + rettangoloGrande.baseRettangolo + " Cm");
+Console.WriteLine("L'altezza del rettangolo classico è di: " + rettangoloGrande.altezzaRettangolo + " Cm");
+
+Rettangolo rettangoloPiccolo = new Rettangolo(10, 5);
+Console.WriteLine("ATTRIBUTI DEL RETTANGOLO PICCOLO: ");
+Console.WriteLine("La base del rettangolo piccolo è di: " + rettangoloPiccolo.baseRettangolo + " Cm");
+Console.WriteLine("L'altezza del rettangolo piccolo è di: " + rettangoloPiccolo.altezzaRettangolo + " Cm");
+
+
---
+++ Geometria/RettangoliVari.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RettangoliVari
+{
+    public class Rettangolo
+    {
+        // ATTRIBUTI / CARATTESTICHE
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+            
+
+        // STATI -
+       
+        
+
+        // COSTRUTTORE
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+
+        }
+
+        // METODI
+    
+        public int calcolaPerimetro()
+        {
+            int perimetroCalcolato = baseRettangolo * altezzaRettangolo;
+            return perimetroCalcolato;
+        }
+
+        
+       
+    }
+}
